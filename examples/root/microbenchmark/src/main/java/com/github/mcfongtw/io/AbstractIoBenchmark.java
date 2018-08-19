0bcb75b82afdd9e49d67ad03472f4b48e148f38c
Michael Fong <mcfong.open@gmail.com>
2018-08-19 13:41:09 +0800

    Add RandomAccessMemoryMapBenchmark
    Add MemoryMapBackedRandomAccessSorting and FileStreamedBackedRandomAccessSorting

--- examples/root/microbenchmark/src/main/java/com/github/mcfongtw/io/AbstractIoBenchmark.java
+++ examples/root/microbenchmark/src/main/java/com/github/mcfongtw/io/AbstractIoBenchmark.java
@@ -8,14 +8,19 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
+
+import static com.github.mcfongtw.io.AbstractIoBenchmark.AbstractRandomAccessExecutionPlan.DataType.*;
 
 public abstract class AbstractIoBenchmark {
 
-    protected static final int NUM_ITERATION = 100;
+    protected static final int NUM_ITERATION = 10;
 
     private static final int UNIT_ONE_KILO = 1024;
 
@@ -145,6 +150,7 @@ public abstract class AbstractIoBenchmark {
             FileUtils.touch(new File(finPath));
             FileUtils.touch(new File(foutPath));
 
+            //Sequential data generation
             try(
                     FileOutputStream fin = new FileOutputStream(finPath);
             ) {
@@ -166,5 +172,161 @@ public abstract class AbstractIoBenchmark {
             logger.debug("Temp dir deleted at [{}]", tempDir.getAbsolutePath());
         }
     }
+
+    protected static class AbstractRandomAccessExecutionPlan extends AbstractExecutionPlan {
+
+        protected String fmetaPath;
+
+        protected String fsummaryPath;
+
+        protected String finPath;
+
+        protected List<String> listOfFoutPath = new ArrayList<>();
+
+        protected File tempDir;
+
+        protected enum DataType {
+            INTEGER(1, 4),
+            DOUBLE(2, 8),
+            CHAR(3, 2),
+            BYTE(4, 1);
+
+            private int type;
+
+            private int sizeOf;
+
+            DataType(int id, int size) {
+                type = id;
+                this.sizeOf = size;
+            }
+
+
+            public static DataType getDataTypeByTypeId(int type) {
+                switch(type) {
+                    case 1:
+                        return INTEGER;
+                    case 2:
+                        return DOUBLE;
+                    case 3:
+                        return CHAR;
+                    case 4:
+                        return BYTE;
+                    default:
+                        return null;
+                }
+            }
+
+            public static DataType getDataTypeBySizeOf(int size) {
+                switch(size) {
+                    case 1:
+                        return BYTE;
+                    case 2:
+                        return CHAR;
+                    case 4:
+                        return INTEGER;
+                    case 8:
+                        return DOUBLE;
+                    default:
+                        return null;
+                }
+            }
+
+            public static int COUNT = DataType.values().length;
+        }
+
+        @Override
+        public void preTrialSetUp() throws Exception {
+            super.preTrialSetUp();
+
+            Random rand = new Random(System.currentTimeMillis());
+
+            tempDir = Files.createTempDir();
+            new File(tempDir.getAbsolutePath()).mkdirs();
+
+            finPath = tempDir.getAbsolutePath() + "/in.data";
+            fmetaPath = tempDir.getAbsolutePath() + "/meta.data";
+            fsummaryPath = tempDir.getAbsolutePath() + "/summary.data";
+
+            FileUtils.touch(new File(finPath));
+            FileUtils.touch(new File(fmetaPath));
+
+            try (
+                    PrintWriter fmeta = new PrintWriter(new FileOutputStream(fmetaPath));
+                    PrintWriter fsummary = new PrintWriter(new FileOutputStream(fsummaryPath));
+                    FileChannel channel = new RandomAccessFile(finPath, "rw").getChannel();
+            ) {
+                int foutFileSize[] = new int[COUNT];
+
+                ByteBuffer buffer = channel.map(FileChannel.MapMode.READ_WRITE, 0, TOTAL_DATA_WRITEN);
+
+                for(int currentIndex = 0; currentIndex < TOTAL_DATA_WRITEN; ) {
+                    DataType nextDataType = getDataTypeByTypeId(rand.nextInt(3) + 1);
+
+                    if(nextDataType.sizeOf + currentIndex >= TOTAL_DATA_WRITEN) {
+                        int remainingByte = TOTAL_DATA_WRITEN - currentIndex;
+                        logger.debug("Remain bytes : [{}]", remainingByte);
+                        nextDataType = DataType.getDataTypeBySizeOf(remainingByte);
+                    }
+
+                    fmeta.println(finPath + "," + nextDataType.type + "," + currentIndex + "," + nextDataType.sizeOf);
+
+                    switch(nextDataType) {
+                        case INTEGER:
+                            buffer = buffer.putInt(currentIndex, rand.nextInt());
+                            foutFileSize[nextDataType.ordinal()]  += nextDataType.sizeOf;
+                            break;
+                        case DOUBLE:
+                            buffer = buffer.putDouble(currentIndex, rand.nextDouble());
+                            foutFileSize[nextDataType.ordinal()]  += nextDataType.sizeOf;
+                            break;
+                        case CHAR:
+                            buffer = buffer.putChar(currentIndex, (char)(rand.nextInt(26) + 'a'));
+                            foutFileSize[nextDataType.ordinal()]  += nextDataType.sizeOf;
+                            break;
+                        case BYTE:
+                            buffer = buffer.put(currentIndex, (byte) rand.nextInt());
+                            foutFileSize[nextDataType.ordinal()]  += nextDataType.sizeOf;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    currentIndex += nextDataType.sizeOf;
+                }
+
+                buffer.flip();
+
+                // Write summary file
+                for (int i = 0; i < foutFileSize.length; i++) {
+                    int size = foutFileSize[i];
+                    fsummary.println(size);
+                }
+            }
+
+            for(int i = 0; i < DataType.COUNT; i++) {
+                String foutPath = tempDir.getAbsolutePath() + "/out-" + DataType.getDataTypeByTypeId(i+1).name() + ".data";
+                FileUtils.touch(new File(foutPath));
+                logger.debug("File created at [{}]", foutPath);
+                listOfFoutPath.add(foutPath);
+            }
+
+            logger.debug("Temp dir created at [{}]", tempDir.getAbsolutePath());
+            logger.debug("File created at [{}]", finPath);
+            logger.debug("File created at [{}]", fmetaPath);
+        }
+
+        @Override
+        public void postTrialTearDown() throws Exception {
+            super.postTrialTearDown();
+
+            FileUtils.deleteDirectory(tempDir);
+            logger.debug("Temp dir deleted at [{}]", tempDir.getAbsolutePath());
+        }
+    }
+
+//    public static void main(String[] args) throws Exception {
+//        AbstractRandomAccessExecutionPlan plan = new AbstractRandomAccessExecutionPlan();
+//        plan.preTrialSetUp();
+//    }
 
 }
---
+++ examples/root/microbenchmark/src/main/java/com/github/mcfongtw/io/RandomAccessMemoryMapBenchmark.java
@@ -0,0 +1,207 @@
+package com.github.mcfongtw.io;
+
+import com.github.mcfongtw.metrics.LatencyMetric;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.github.mcfongtw.io.AbstractIoBenchmark.AbstractRandomAccessExecutionPlan.DataType.COUNT;
+
+public class RandomAccessMemoryMapBenchmark extends AbstractIoBenchmark {
+
+    public static Logger LOG = LoggerFactory.getLogger(RandomAccessMemoryMapBenchmark.class);
+
+    @State(Scope.Benchmark)
+    public static class RandomAccessMemoryMapExecutionPlan extends AbstractRandomAccessExecutionPlan {
+
+        LatencyMetric ioLatencyMetric = new LatencyMetric(RandomAccessMemoryMapExecutionPlan.class.getName());
+
+        @Override
+        @Setup(Level.Trial)
+        public void doTrialSetUp() throws Exception {
+            super.doTrialSetUp();
+        }
+
+        @Override
+        @TearDown(Level.Trial)
+        public void doTrialTearDown() throws Exception {
+            super.doTrialTearDown();
+        }
+
+        @Override
+        @Setup(Level.Iteration)
+        public void doIterationSetup() throws Exception {
+            super.doIterationSetup();
+        }
+
+        @Override
+        @TearDown(Level.Iteration)
+        public void doIterationTearDown() throws Exception {
+            super.doIterationSetup();
+        }
+
+    }
+
+    @Benchmark
+    @BenchmarkMode({Mode.AverageTime, Mode.SampleTime})
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @Measurement(iterations = NUM_ITERATION, time = 800, timeUnit = TimeUnit.MILLISECONDS)
+    public void doMemoryMapBackedRandomAccessSorting(RandomAccessMemoryMapExecutionPlan plan) throws IOException {
+        try (
+                RandomAccessFile fin = new RandomAccessFile(plan.finPath, "r");
+                BufferedReader fmeta = new BufferedReader(new FileReader(plan.fmetaPath));
+                BufferedReader fsummary = new BufferedReader(new FileReader(plan.fsummaryPath));
+
+                FileChannel finChannel = fin.getChannel();
+        ) {
+
+            int foutFileSize[] = new int[COUNT];
+            for(int i = 0; i < foutFileSize.length; i++) {
+                foutFileSize[i] = Integer.valueOf(fsummary.readLine());
+            }
+
+            int finLength = (int) finChannel.size();
+
+            MappedByteBuffer bufIn = finChannel.map(FileChannel.MapMode.READ_ONLY, 0, finLength);
+
+            List<RandomAccessFile> listOfRandomAccessFile = new ArrayList<>();
+            List<FileChannel> listOfFileChannel = new ArrayList<>();
+            List<MappedByteBuffer> listOfMappedOutputBuffer = new ArrayList<>();
+            for(int i = 0; i < plan.listOfFoutPath.size(); i++) {
+                String fout = plan.listOfFoutPath.get(i);
+                RandomAccessFile raf = new RandomAccessFile(fout, "rw");
+                FileChannel fc = raf.getChannel();
+                MappedByteBuffer mbb = fc.map(FileChannel.MapMode.READ_WRITE, 0, foutFileSize[i]);
+                listOfRandomAccessFile.add(raf);
+                listOfFileChannel.add(fc);
+                listOfMappedOutputBuffer.add(mbb);
+            }
+
+            long beforeTime = System.nanoTime();
+
+            String line = null;
+            while( (line = fmeta.readLine()) != null) {
+                String parts[] = line.split(",");
+                String fpath = parts[0];
+                AbstractRandomAccessExecutionPlan.DataType dataType = AbstractRandomAccessExecutionPlan.DataType.getDataTypeByTypeId(Integer.valueOf(parts[1]));
+                int index = Integer.valueOf(parts[2]);
+                int length = Integer.valueOf(parts[3]);
+
+                LOG.debug("path: [{}] | datatype: [{}] | index: [{}] | length: [{}]", new Object[]{fpath, dataType.name(), index, length});
+
+                byte[] data = new byte[length];
+                bufIn.get(data, 0, length);
+                MappedByteBuffer bufOut = listOfMappedOutputBuffer.get(dataType.ordinal());
+                bufOut.put(data);
+            }
+
+            long afterTime = System.nanoTime();
+            plan.ioLatencyMetric.addTime(afterTime - beforeTime, TimeUnit.NANOSECONDS);
+
+
+
+            //Assert sum of ALL{fout.length} == fin.length
+            int sumOfFoutLength = 0;
+            for(FileChannel fc: listOfFileChannel) {
+                sumOfFoutLength += fc.size();
+            }
+            assert fin.length() == sumOfFoutLength;
+
+            for(FileChannel fc: listOfFileChannel) {
+                fc.close();
+            }
+            for(RandomAccessFile raf : listOfRandomAccessFile) {
+                raf.close();
+            }
+        }
+
+    }
+
+    @Benchmark
+    @BenchmarkMode({Mode.AverageTime, Mode.SampleTime})
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @Measurement(iterations = NUM_ITERATION, time = 500, timeUnit = TimeUnit.MILLISECONDS)
+    public void doFileStreamedBackedRandomAccessSorting(RandomAccessMemoryMapExecutionPlan plan) throws IOException {
+        try (
+                FileInputStream fis = new FileInputStream(plan.finPath);
+                BufferedReader fmeta = new BufferedReader(new FileReader(plan.fmetaPath));
+                BufferedReader fsummary = new BufferedReader(new FileReader(plan.fsummaryPath));
+        ) {
+
+            int foutFileSize[] = new int[COUNT];
+            for(int i = 0; i < foutFileSize.length; i++) {
+                foutFileSize[i] = Integer.valueOf(fsummary.readLine());
+            }
+
+
+            List<FileOutputStream> listOfFileOutputStream = new ArrayList<>();
+            for(int i = 0; i < plan.listOfFoutPath.size(); i++) {
+                String fout = plan.listOfFoutPath.get(i);
+                FileOutputStream fos = new FileOutputStream(fout);
+                listOfFileOutputStream.add(fos);
+            }
+
+
+            long beforeTime = System.nanoTime();
+
+            String line = null;
+            while( (line = fmeta.readLine()) != null) {
+                String parts[] = line.split(",");
+                String fpath = parts[0];
+                AbstractRandomAccessExecutionPlan.DataType dataType = AbstractRandomAccessExecutionPlan.DataType.getDataTypeByTypeId(Integer.valueOf(parts[1]));
+                int index = Integer.valueOf(parts[2]);
+                int length = Integer.valueOf(parts[3]);
+
+                LOG.debug("path: [{}] | datatype: [{}] | index: [{}] | length: [{}]", new Object[]{fpath, dataType.name(), index, length});
+
+                byte[] data = new byte[length];
+                fis.read(data, 0, length);
+                FileOutputStream fos = listOfFileOutputStream.get(dataType.ordinal());
+                fos.write(data);
+            }
+
+            long afterTime = System.nanoTime();
+            plan.ioLatencyMetric.addTime(afterTime - beforeTime, TimeUnit.NANOSECONDS);
+
+
+
+            //Assert sum of ALL{fout.length} == fin.length
+            int sumOfFoutLength = 0;
+            for(FileOutputStream fos: listOfFileOutputStream) {
+                sumOfFoutLength += fos.getChannel().size();
+            }
+            assert fis.getChannel().size() == sumOfFoutLength;
+
+            for(FileOutputStream fos: listOfFileOutputStream) {
+                fos.close();
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        //TODO: Need to recreate table via command line:
+        //curl -XPOST 'http://localhost:8086/query' --data-urlencode 'q=DROP DATABASE "demo"'
+        //curl -XPOST 'http://localhost:8086/query' --data-urlencode 'q=CREATE DATABASE "demo"'
+        Options opt = new OptionsBuilder()
+                .include(RandomAccessMemoryMapBenchmark.class.getSimpleName())
+                .detectJvmArgs()
+                .warmupIterations(0)
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+}
