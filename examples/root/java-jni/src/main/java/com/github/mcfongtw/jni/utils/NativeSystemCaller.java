f98fc33fec99bbd32cc9446efe6893a0872994e3
Michael Fong <mcfong.open@gmail.com>
2018-09-25 22:44:23 +0800

    Add NativeSystemCaller
    Add jni/cpp/java for NativeSystemCaller
    
    Add jni/native dependency for microbenchmark

---
+++ examples/root/java-jni/src/main/java/com/github/mcfongtw/jni/utils/NativeSystemCaller.java
@@ -0,0 +1,23 @@
+package com.github.mcfongtw.jni.utils;
+
+public class NativeSystemCaller {
+
+    static {
+        JniUtils.loadLibrary("libnative.so");
+    }
+
+    private NativeSystemCaller() {
+        //avoid instantiation
+    }
+
+    public static final int MCL_CURRENT = 1;
+
+    public static final int MCL_FUTURE = 2;
+
+    //javah -o ../../../native/src/main/cpp/NativeSystemCaller.h com.github.mcfongtw.jni.utils.NativeSystemCaller
+
+    public static native int mlockall(int flags);
+
+    public static native int munlockall();
+
+}
