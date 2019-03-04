e4b9cad0b4827b9598a949e25e1de1cc0c7f0b3a
Michael Fong <mcfong.open@gmail.com>
2019-03-04 17:26:26 +0800

    Add a MetricConccurrentTests
    Add a test that verifies concurrency against incrementing Counter value without resetting.

--- examples/root/common/src/main/java/com/github/mcfongtw/metrics/MetricUtils.java
+++ examples/root/common/src/main/java/com/github/mcfongtw/metrics/MetricUtils.java
@@ -1,7 +1,9 @@
 package com.github.mcfongtw.metrics;
 
+import com.codahale.metrics.Counter;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.SharedMetricRegistries;
+import com.google.common.annotations.VisibleForTesting;
 
 public class MetricUtils {
 
@@ -19,5 +21,21 @@ public class MetricUtils {
 
     public static String concat(String prefix, String metricName) {
         return prefix + "." + metricName;
+    }
+
+    public static void incCounter(Counter counter, long newVal) {
+        counter.inc(newVal - counter.getCount());
+    }
+
+    @VisibleForTesting
+    /*
+     * There is a chance that a reader thread could Counter.getCount() == 0;
+     *
+     * For details, check com.github.mcfongtw.metrics.MetricConcurrentTest#testUnsafeIncCounter
+     */
+    static void unsafeIncCounter(Counter counter, long newVal) {
+        long oldVal = counter.getCount();
+        counter.dec(oldVal);
+        counter.inc(newVal);
     }
 }
