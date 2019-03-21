9b2eb93ba9ad0c7f0963748ab12e460cebd10f9c
Michael Fong <mcfong.open@gmail.com>
2019-03-21 22:17:05 +0800

    Update FibonacciBenchmark with Java8 / RxJava / Reactor Stream
    Update Fibonacci function with Java8, RxJava and Reactor Stream
    
    Verify Fibonacci functions with unit test

--- examples/root/microbenchmark/src/main/java/com/github/mcfongtw/FibonacciBenchmark.java
+++ examples/root/microbenchmark/src/main/java/com/github/mcfongtw/FibonacciBenchmark.java
@@ -1,35 +1,57 @@
 package com.github.mcfongtw;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.Lists;
+import io.reactivex.Observable;
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.profile.GCProfiler;
 import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
+import reactor.core.publisher.Flux;
+import reactor.util.function.Tuples;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
 public class FibonacciBenchmark {
 
-    private static final int STARTING_FIB_NUMBER = 45;
+    private static final int NUM_ITERATION = 1;
 
-    private int fibNaiveRecursive(int x) {
-        return (x == 1 || x == 2)?1:fibNaiveRecursive(x - 1) + fibNaiveRecursive(x - 2);
+    @State(Scope.Benchmark)
+    public static class ExecutionPlan {
+        @Param({"10", "50"})
+        public int FIB_NUMBER_INDEX;
     }
 
-    private int fibTailRecursive(int x) {
-        return fibTailRec(x, 0,1);
+    /*          1              , if x = 1
+     * f(x) =   1              , if x = 2
+     *        f(x-1) + f(x - 2), else
+     */
+    @VisibleForTesting
+    static int doRecursion(int x) {
+        return (x == 0 || x == 1) ? x : doRecursion(x - 1) + doRecursion(x - 2);
     }
 
-    private int fibTailRec(int n, int a, int b) {
-        if (n == 0) return a;
-        if (n == 1) return b;
-        return fibTailRec(n - 1, b, a + b);
+    @VisibleForTesting
+    static int doTailRecursive(int x) {
+        if(x <= 2)
+            return 2;
+        else
+            return fibTailRec(x, 0,1);
     }
 
-    private int fibMemoization(int x, int[] mem) {
+    private static int fibTailRec(int x, int fMinus1, int fMinus2) {
+        if(x == 0) return fMinus1;
+        return fibTailRec(x - 1, fMinus2, fMinus1 + fMinus2);
+    }
+
+
+    private static int fibMemoization(int x, int[] mem) {
         if (mem[x] != 0) return mem[x];
         if (x == 1 || x == 2)  return 1;
         int n = fibMemoization(x - 1, mem) + fibMemoization(x - 2,mem);
@@ -37,8 +59,15 @@ public class FibonacciBenchmark {
         return n;
     }
 
-    private int fibBottomUp(int x) {
-        if (x == 1 || x == 2) return 1;
+    @VisibleForTesting
+    static int doDpTopDown(int x) {
+        return fibMemoization(x, new int[x + 1]);
+    }
+
+
+    @VisibleForTesting
+    static int doDpBottomUp(int x) {
+        if (x == 0 || x == 1) return 1;
         int[] memory = new int[x + 1];
         memory[1] = 1;
         memory[2] = 1;
@@ -46,14 +75,43 @@ public class FibonacciBenchmark {
         return memory[x];
     }
 
-    private int fibStream(int n) {
+    @VisibleForTesting
+    static int doJava8Stream(int n) {
         return Stream.iterate(new Integer[]{0, 1}, s -> new Integer[]{s[1], s[0]+s[1]})
                 .limit(n)
                 .reduce((x, y) -> y).orElse(null)[1];
     }
 
+    @VisibleForTesting
+    static int doRxStream(int x) {
+        return Observable.fromArray(0)
+                .repeat()
+                .scan(new int[]{0, 1}, (a, b) -> new int[]{a[1], a[0] + a[1]})
+                .map(a -> a[1])
+                .take(x)
+                .blockingLast();
+    }
 
-    public int fibIterative(int n) {
+
+    @VisibleForTesting
+    static int doReactorStream(int x) {
+        List<Integer> sequence = Lists.newArrayList();
+        Flux<Integer> integerFlux = Flux.generate(
+                () -> Tuples.<Integer, Integer>of(1, 1),
+                (state, sink) -> {
+                    sink.next(state.getT1());
+                    return Tuples.of(state.getT2(), state.getT1() + state.getT2());
+                });
+
+        integerFlux
+                .take(x)
+                .subscribe(number -> sequence.add(number));
+        return sequence.get(x - 1);
+    }
+
+
+    @VisibleForTesting
+    static int doIterative(int n) {
         if(n <= 1) {
             return n;
         }
@@ -68,60 +126,78 @@ public class FibonacciBenchmark {
         return fib;
     }
 
-    @Benchmark
-    @BenchmarkMode({Mode.Throughput, Mode.AverageTime})
-    @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    @Measurement(iterations=10)
-    public void naiveIterative() {
-        fibIterative(STARTING_FIB_NUMBER);
-    }
+
 
 
     @Benchmark
-    @BenchmarkMode({Mode.Throughput, Mode.AverageTime})
-    @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    @Measurement(iterations=10)
-    public void naiveRecursive() {
-        fibNaiveRecursive(STARTING_FIB_NUMBER);
+    @BenchmarkMode({Mode.Throughput})
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @Measurement(iterations=NUM_ITERATION)
+    public void measureRecursive(ExecutionPlan executionPlan, Blackhole blackhole) {
+        blackhole.consume(doRecursion(executionPlan.FIB_NUMBER_INDEX));
     }
 
     @Benchmark
-    @BenchmarkMode({Mode.Throughput, Mode.AverageTime})
-    @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    @Measurement(iterations=10)
-    public void tailRecursive(){
-        fibTailRecursive(STARTING_FIB_NUMBER);
+    @BenchmarkMode({Mode.Throughput})
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @Measurement(iterations=NUM_ITERATION)
+    public void measureTailRecursive(ExecutionPlan executionPlan, Blackhole blackhole){
+        blackhole.consume(doTailRecursive(executionPlan.FIB_NUMBER_INDEX));
     }
 
     @Benchmark
-    @BenchmarkMode({Mode.Throughput, Mode.AverageTime})
-    @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    @Measurement(iterations=10)
-    public void memoization() {
-        fibMemoization(STARTING_FIB_NUMBER,new int[STARTING_FIB_NUMBER+1]);
+    @BenchmarkMode({Mode.Throughput})
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @Measurement(iterations=NUM_ITERATION)
+    public void measureIterative(ExecutionPlan executionPlan, Blackhole blackhole) {
+        blackhole.consume(doIterative(executionPlan.FIB_NUMBER_INDEX));
     }
 
     @Benchmark
-    @BenchmarkMode({Mode.Throughput, Mode.AverageTime})
-    @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    @Measurement(iterations=10)
-    public void bottomUp() {
-        fibBottomUp(STARTING_FIB_NUMBER);
+    @BenchmarkMode({Mode.Throughput})
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @Measurement(iterations=NUM_ITERATION)
+    public void measureDynamicProgrammingTopDown(ExecutionPlan executionPlan, Blackhole blackhole) {
+        blackhole.consume(doDpTopDown(executionPlan.FIB_NUMBER_INDEX));
     }
 
     @Benchmark
-    @BenchmarkMode({Mode.Throughput, Mode.AverageTime})
-    @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    @Measurement(iterations=10)
-    public void stream() {
-        fibStream(STARTING_FIB_NUMBER);
+    @BenchmarkMode({Mode.Throughput})
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @Measurement(iterations=NUM_ITERATION)
+    public void measureDynamicProgrammingBottomUp(ExecutionPlan executionPlan, Blackhole blackhole) {
+        blackhole.consume(doDpBottomUp(executionPlan.FIB_NUMBER_INDEX));
+    }
+
+    @Benchmark
+    @BenchmarkMode({Mode.Throughput})
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @Measurement(iterations=NUM_ITERATION)
+    public void measureJavaStream(ExecutionPlan executionPlan, Blackhole blackhole) {
+        blackhole.consume(doJava8Stream(executionPlan.FIB_NUMBER_INDEX));
+    }
+
+    @Benchmark
+    @BenchmarkMode({Mode.Throughput})
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @Measurement(iterations=NUM_ITERATION)
+    public void measureRxStream(ExecutionPlan executionPlan, Blackhole blackhole) {
+        blackhole.consume(doRxStream(executionPlan.FIB_NUMBER_INDEX));
+    }
+
+    @Benchmark
+    @BenchmarkMode({Mode.Throughput})
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @Measurement(iterations=NUM_ITERATION)
+    public void measureReactorStream(ExecutionPlan executionPlan, Blackhole blackhole) {
+        blackhole.consume(doReactorStream(executionPlan.FIB_NUMBER_INDEX));
     }
 
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
                 .include(FibonacciBenchmark.class.getSimpleName())
-                .forks(1)
-                .warmupIterations(10)
+                .forks(3)
+                .warmupIterations(5)
                 .addProfiler(GCProfiler.class)
                 .resultFormat(ResultFormatType.JSON)
                 .result("FibonacciBenchmark-result.json")
